eea920fed43a3361508cfeb453b5b4274e675ae1
Gabriel Souza <gabriel.souza@mesp.pt>
2024-01-04 11:17:27 +0000

    Servidor HTML Simples com NODE_JS

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 
-
 app.use('/', express.static('public'));
 
 const PORT = process.env.PORT || 8085;
 app.listen(PORT, function() {
-  console.log( `App escutando na porta ${PORT}`);
+  console.log( `App on link http://localhost:${PORT}`);
 });
